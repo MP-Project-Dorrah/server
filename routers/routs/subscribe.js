1c749f6f3e60a03ae32928553a88df9e159d588f
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-12-21 17:17:37 +0300

    get One Subscribe

--- routers/routs/subscribe.js
+++ routers/routs/subscribe.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const subscribeRouter = express.Router();
 
-const { payment, cancelPayment } = require("./../controllers/subscribe");
+const { payment, cancelPayment  , getOneSubscribe} = require("./../controllers/subscribe");
 
 subscribeRouter.post("/payment", payment);
 subscribeRouter.put("/delete", cancelPayment);
+subscribeRouter.get("/:userId", getOneSubscribe);
 
 module.exports = subscribeRouter;
